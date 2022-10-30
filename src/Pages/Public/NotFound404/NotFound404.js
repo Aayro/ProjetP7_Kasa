3b2fd6d16c3594b00f01b6df7f25109bd9d2ba85
ItsNootMe <axelmayan@gmail.com>
2022-10-30 18:01:35 +0100

    MODIF

--- src/Pages/Public/NotFound404/NotFound404.js
+++ src/Pages/Public/NotFound404/NotFound404.js
@@ -4,13 +4,16 @@ import { NavLink } from 'react-router-dom';
 import "@/Pages/Public/NotFound404/NotFound404.css";
 
 const Erreur404 = () => {
+
     return (
-        <section className="Erreur404">
-            <h1 className="error_404">404</h1>
-            <p className="p_404">Oups! La page que vous avez demandez n'existe pas.</p>
-            <NavLink to="/home" className="navlink_404">Retourner sur la page d'accueil.</NavLink>
-        </section>
-    )
+        <>
+            <div className="page-404">
+                <h1 className="titre-404">404</h1>
+                <span className="description-404">Oups! La page que vous demandez n'existe pas.</span>
+                <NavLink to="/" className="lien-site lien-404">Retourner sur la page d’accueil</NavLink>
+            </div>
+        </>
+    );
 }
 
 export default Erreur404;
